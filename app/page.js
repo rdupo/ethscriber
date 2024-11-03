aedf0904258c544a01ef1c43a553db46074cf12c
rdupo <67809219+rdupo@users.noreply.github.com>
2024-11-03 12:23:04 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -57,9 +57,10 @@ export default function Home() {
   return (
     <div className="bg-grey-900 flex flex-col h-screen justify-between">
       <Header/>
+      <span className="bg-pink-500 text-white pl-4 py-2">🚨 This site is for QA purposes only - please confirm you are on Sepolia testnet before sending any transactions! 🚨</span>
       <div className="mb-auto h-10 ml-4 my-6 w-5/6">
         <h1 className="text-white text-2xl">(PH)REMIX.2174</h1>
-        <p className="text-white text-l">A collection of xxx CryptoPhunk remixes inspired by sampling in hip-hop beats, (PH)REMIXes are the next iteration of Phunk Remixes by <a href="https://x.com/0xDuplo" target="_blank" className="text-pink-500">Duplo</a>. While the first remixes we all manually created, (PH)REMIXes are computer assisted, with pixels randomly placed in a specified region (or regions) of the origional 24x24 grid, starting with <a href="https://www.vphree.io/cryptophunk/2174" target="_blank" className="text-pink-500">CryptoPhunk 2174</a>.</p>
+        <p className="text-white text-l">A collection of 100 CryptoPhunk remixes inspired equally by CryptoPhunk artwork and the sampling commonly found in hip-hop production, (PH)REMIXes are the next iteration of Phunk Remixes by <a href="https://x.com/0xDuplo" target="_blank" className="text-pink-500">Duplo</a>. While the first remixes we all manually created, (PH)REMIXes are computer assisted, with pixels randomly placed in a specified region (or regions) of the origional 24x24 grid, starting with <a href="https://www.vphree.io/cryptophunk/2174" target="_blank" className="text-pink-500">CryptoPhunk 2174</a>.</p>
         <br/>
         <p className="text-white text-l mb-6">To <a href="https://ethscriptions.com/" target="_blank" className="text-pink-500">Ethscribe</a> a (PH)REMIX, simple click the image and confirm the transaction. Simple as that.</p>
         <div className="flex flex-wrap justify-start pb-80">
@@ -73,7 +74,7 @@ export default function Home() {
                 data={phunk.data}
               />
               : 
-              <p>Loading...</p> )  
+              null)  
           ))}
         </div>
       </div>
