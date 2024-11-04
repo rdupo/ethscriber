0cad15590aa77be96b922c5352021f77d2fa8264
rdupo <67809219+rdupo@users.noreply.github.com>
2024-11-04 10:34:09 -0600

    maintenance screen

--- app/page.js
+++ app/page.js
@@ -57,14 +57,16 @@ export default function Home() {
   return (
     <div className="bg-grey-900 flex flex-col h-screen justify-between">
       <Header/>
-      <span className="bg-pink-500 text-white pl-4 py-2">🚨 This site is for QA purposes only - please confirm you are on Sepolia testnet before sending any transactions! 🚨</span>
+      <span className="bg-pink-500 text-white pl-4 py-2">🚧 Down for maintenance 🚧</span>
+      {/*<span className="bg-pink-500 text-white pl-4 py-2">🚨 This site is for QA purposes only - please confirm you are on Sepolia testnet before sending any transactions! 🚨</span>*/}
       <div className="mb-auto h-10 my-6">
-        <h1 className="ml-4 w-11/12 text-white text-2xl mb-2">(PH)REMIX.2174</h1>
-        <p className="ml-4 w-11/12 text-white text-l">A collection of 100 CryptoPhunk remixes inspired equally by CryptoPhunk artwork and the sampling commonly found in hip-hop production, (PH)REMIXes are the next iteration of Phunk Remixes by <a href="https://x.com/0xDuplo" target="_blank" className="text-pink-500">Duplo</a>. While the first remixes we all manually created, (PH)REMIXes are computer assisted, with pixels randomly placed in a specified region (or regions) of the origional 24x24 grid, starting with <a href="https://www.vphree.io/cryptophunk/2174" target="_blank" className="text-pink-500">CryptoPhunk 2174</a>.</p>
+        {/*revert all gray-700 text below back to white, pink-900 to pink-500*/}
+        <h1 className="ml-4 w-11/12 text-gray-700 text-2xl mb-2">(PH)REMIX.2174</h1>
+        <p className="ml-4 w-11/12 text-gray-700 text-l">A collection of 100 CryptoPhunk remixes inspired equally by CryptoPhunk artwork and the sampling commonly found in hip-hop production, (PH)REMIXes are the next iteration of Phunk Remixes by <a href="https://x.com/0xDuplo" target="_blank" className="text-pink-900">Duplo</a>. While the first remixes we all manually created, (PH)REMIXes are computer assisted, with pixels randomly placed in a specified region (or regions) of the origional 24x24 grid, starting with <a href="https://www.vphree.io/cryptophunk/2174" target="_blank" className="text-pink-900">CryptoPhunk 2174</a>.</p>
         <br/>
-        <p className="ml-4 w-11/12 text-white text-l mb-6">To <a href="https://ethscriptions.com/" target="_blank" className="text-pink-500">Ethscribe</a> a (PH)REMIX just click the image and confirm the transaction from your wallet. Simple as that.</p>
+        <p className="ml-4 w-11/12 text-gray-700 text-l mb-6">To <a href="https://ethscriptions.com/" target="_blank" className="text-pink-900">Ethscribe</a> a (PH)REMIX just click the image and confirm the transaction from your wallet. Simple as that.</p>
         <div className="flex flex-wrap justify-center pb-80">
-          {pr.map((phunk) => (
+          {/*pr.map((phunk) => (
             (typeof(phunk.id) != 'undefined' ?
               <Card
                 id={phunk.id}
@@ -75,7 +77,7 @@ export default function Home() {
               />
               : 
               null)  
-          ))}
+          ))*/}
         </div>
       </div>
       <Footer/>
